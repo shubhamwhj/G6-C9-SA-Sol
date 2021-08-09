42c831ff2cf6e97c7ff90496b359aba0eea9a003
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-08-09 15:45:02 +0530

    Update sketch.js

--- P5 JS SPRITES/sketch.js
+++ P5 JS SPRITES/sketch.js
@@ -4,9 +4,9 @@ function setup()
  createCanvas(400,400)
  box = createSprite(40,40,40,40);
  box.shapeColor = color(255);
- box.position.x=100;
+ box.position.x=0;
  box.position.y=150;
- box.velocity.y=0.5
+ box.velocityX=5
 }
 
 function draw()
